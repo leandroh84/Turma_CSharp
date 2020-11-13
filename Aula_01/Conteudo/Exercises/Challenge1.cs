a25741dd1e25bb61efb89791f9664e0fa7414a1f
LeandroPereira <leandropereiradez@gmail.com>
2020-11-12 21:39:28 -0500

    test

--- Aula_01/Conteudo/Exercises/Challenge1.cs
+++ Aula_01/Conteudo/Exercises/Challenge1.cs
@@ -14,7 +14,6 @@ namespace Aula_01.Conteudo.Exercises
             double x1 = (-b + (Math.Sqrt(Math.Pow(b, 2) - 4 * a * c))) / (2 * a);
             double x2 = (-b - (Math.Sqrt(Math.Pow(b, 2) - 4 * a * c))) / (2 * a);
 
-
             Console.WriteLine("x1= " +x1+ "\nx2= " +x2);
         }
     }
